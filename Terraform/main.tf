9488de076b8957753d5a2bff67a430fb5892e6dc
mdumala <dumala.mikolaj@gmail.com>
2023-10-26 09:37:40 +0200

    Create first bucket on AWS

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region  = "us-west-1" # Change this to your preferred region
+}
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "TF-K8S-BUCKET"
+  acl    = "private"
+
+  tags = {
+    type = "k8s"
+  }
+
+
+}
